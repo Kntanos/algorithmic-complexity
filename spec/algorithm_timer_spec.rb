65b38bd2c7caac07ca6563213a85174b7c1091a3
Kostas Ntanos <kostas@ntanos.co.uk>
2021-12-18 17:08:22 +0000

    Refactor test to mock rand

--- spec/algorithm_timer_spec.rb
+++ spec/algorithm_timer_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe AlgorithmTimer do
   describe '#setup' do
     it 'returns an array with the given number of elements' do
       test = AlgorithmTimer.new('some_fucntion')
+      allow(test).to receive(:rand).and_return(1, 2, 3)
 
-      expect(test.setup(10).length).to eq(10)
-      expect(test.setup(1000).length).to eq(1000)
+      expect(test.setup(3)).to eq([1, 2, 3])
     end
   end
 end
