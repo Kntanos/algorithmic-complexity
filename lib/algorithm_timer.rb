8ff4ddbc6ca02101a23c33b71766395d66c3375f
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-12-18 13:48:50 +0000

    REFACTOR RENAME CLASS
    Changed the structure of the class, so the methods operate only on one sample set
    at a time instead on all at once. This makes possible to run different number of
    throwaway tests for each sample size and also repeat separately any tests that
    may have problematic data output.

---
+++ lib/algorithm_timer.rb
@@ -0,0 +1,40 @@
+class AlgorithmTimer
+
+  attr_reader :function, :data
+
+  def initialize(function)
+    @function = function.to_sym
+    @data = []
+  end
+
+  def setup(sample_size)
+    @test_array = Array.new(sample_size) { rand(0...9) }
+  end
+  
+  def throwaway_data(throwaway_repeats)
+    throwaway_repeats.times do
+      @test_array.method(function)
+    end
+  end
+  
+  def timer 
+    start_time = Time.now 
+    @test_array.method(function)
+    stop_time = Time.now
+
+    @time = (stop_time - start_time) * 1000.0 # in milliseconds
+  end
+
+  def run_test(sample_size, throwaway_repeats, repeats)
+    setup(sample_size)
+    throwaway_data(throwaway_repeats)
+    repeats.times do
+      timer
+      
+    end
+  end
+
+  def export_csv
+    [sample_size, @time] # todo
+  end
+end
