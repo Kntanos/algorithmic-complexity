d08df950b07af080296aa90fc3d34947e2ed79eb
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-12-21 12:31:05 +0000

    Insert constants for SAMPLE-SIZE and THROWAWAY

--- lib/algorithm_timer.rb
+++ lib/algorithm_timer.rb
@@ -1,8 +1,10 @@
 require 'csv'
 
 class AlgorithmTimer
-
   attr_reader :function, :data, :test_array, :time
+
+  SAMPLE_SIZES = [10, 20, 30]
+  THROWAWAY = 10
 
   def initialize(function)
     @function = function.to_sym
@@ -13,8 +15,8 @@ class AlgorithmTimer
     @test_array = Array.new(sample_size) { rand(0...9) }
   end
   
-  def throwaway_data(throwaway_repeats)
-    throwaway_repeats.times do
+  def throwaway_data
+    THROWAWAY.times do
       test_array.method(function)
     end
   end
@@ -33,20 +35,31 @@ class AlgorithmTimer
     data.push(time)
   end
 
-  def run_test(sample_size, throwaway_repeats, repeats)
+  def average_data
+    data.sum(0.0)/data.size
+  end
+
+  def run_test(sample_size, repeats)
     setup(sample_size)
-    throwaway_data(throwaway_repeats)
+    throwaway_data
     repeats.times do
       timer
       time_diff
       collect_data
     end
     export_csv
+    data.clear
+  end
+
+  def run_together(repeats)
+    SAMPLE_SIZES.each do |sample_size|
+      run_test(sample_size, repeats)
+    end
   end
 
   def export_csv
-    CSV.open("#{test_array.length}_test_data.csv", 'w') do |csv|
-      csv << (data.unshift(test_array.length))
+    CSV.open("test_data.csv", 'a') do |csv|
+      csv << ([average_data].unshift(test_array.length))
     end
   end
 end
