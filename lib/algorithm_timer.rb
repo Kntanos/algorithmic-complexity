3f1fc076e0ba876f9e83b65db8e85a0e4689bbe4
Kostas Ntanos <kostas@ntanos.co.uk>
2021-12-22 17:35:26 +0000

    Fix the timer

--- lib/algorithm_timer.rb
+++ lib/algorithm_timer.rb
@@ -1,9 +1,9 @@
 require 'csv'
 
 class AlgorithmTimer
-  attr_reader :function, :data, :test_array, :time
+  attr_reader :function, :data#, :test_array
 
-  SAMPLE_SIZES = [*1..20].map{ |n| n * 50000}
+  SAMPLE_SIZES = [*1..2].map{ |n| n * 5000}
   THROWAWAY = 100
 
   def initialize(function)
@@ -17,53 +17,71 @@ class AlgorithmTimer
   
   def throwaway_data
     THROWAWAY.times do
-      test_array.method(function)
+      @test_array.method(function).call
     end
   end
   
   def timer 
-    @start_time = Time.now 
-    test_array.method(function)
-    @stop_time = Time.now
+    start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    @test_array.method(function).call
+    stop = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+    @time = (stop - start) * 1000.0
   end
 
-  def time_diff
-    @time = (@stop_time - @start_time) * 1000.0 # in milliseconds
-  end
+  # def time_diff
+  #   @time = (@stop_time - @start_time) * 1000.0 # in milliseconds
+  # end
 
   def collect_data
-    data.push(time)
+    data.push(@time)
   end
 
-  def average_data
-    remove_pc = 0.05
-    remove_min_max = data.length*remove_pc...data.length*(1 - remove_pc)
-    pc_data_remaining = 1 - 2*remove_pc
+  # def average_data
+  #   remove_pc = 0.05
+  #   remove_min_max = data.length*remove_pc...data.length*(1 - remove_pc)
+  #   pc_data_remaining = 1 - 2*remove_pc
 
-    (data.sort[remove_min_max]).sum(0.0) / data.size * pc_data_remaining # First remove 5% of top and bottom
-  end
+  #   (data.sort[remove_min_max]).sum(0.0) / data.size * pc_data_remaining # First remove 5% of top and bottom
+  # end
 
-  def run_test(sample_size, repeats) # Keeping this method in case a sample size needs to be repeated separately
-    setup(sample_size)
-    throwaway_data
-    repeats.times do
-      timer
-      time_diff
-      collect_data
+  def calc_median
+    return nil if data.empty?
+    sorted = data.sort
+    len = sorted.length
+    (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
     end
-    export_csv(average_data)
-    data.clear
-  end
+
+  # def run_test(sample_size, repeats) # Keeping this method in case a sample size needs to be repeated separately
+  #   setup(sample_size)
+  #   throwaway_data
+  #   repeats.times do
+  #     timer
+  #     time_diff
+  #     collect_data
+  #   end
+  #   export_csv(calc_median)
+  #   data.clear
+  # end
 
   def run_all_samples(repeats)
     SAMPLE_SIZES.each do |sample_size|
-      run_test(sample_size, repeats)
+      # run_test(sample_size, repeats)
+      setup(sample_size)
+    throwaway_data
+    repeats.times do
+      timer
+      # time_diff
+      collect_data
+    end
+    export_csv(calc_median)
+    data.clear
     end
   end
 
-  def export_csv(average_data)
+  def export_csv(median)
     CSV.open("#{function}_data.csv", 'a') do |csv|
-      csv << [average_data].unshift(test_array.length)
+      csv << [calc_median].unshift(@test_array.length)
     end
   end
 end
