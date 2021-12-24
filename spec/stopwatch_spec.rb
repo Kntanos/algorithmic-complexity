029b1302fda5ca1e3d315eda0c785251a0abdb02
Kostas Ntanos <kostas@ntanos.co.uk>
2021-12-24 13:14:12 +0000

    Pass .throwaway_data test with stub for :function

--- spec/stopwatch_spec.rb
+++ spec/stopwatch_spec.rb
@@ -2,14 +2,15 @@ require 'stopwatch'
 
 RSpec.describe Stopwatch do
   describe '.throwaway_data' do
+    let(:function){'function'}
 
     it 'runs the given method THROWAWAY_RUNS times for a given sample' do
       sample = double('sample')
-      allow(sample).to receive(:last)
+      allow(sample).to receive(:function)
 
-      subject.throwaway_data(sample, 'last')
+      subject.throwaway_data(sample, function)
 
-      expect(sample).to have_received(:last).exactly(Stopwatch::THROWAWAY_RUNS).times
+      expect(sample).to have_received(:function).exactly(Stopwatch::THROWAWAY_RUNS).times
     end
   end
 end
